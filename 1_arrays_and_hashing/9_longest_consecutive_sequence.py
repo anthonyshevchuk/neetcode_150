d15775dfe12dfcc186396683951ab50dfbd61782
Anton Shevchuk <anthonyshevchuk@gmail.com>
2023-06-18 10:38:48 +0300

    add two test case explanations

--- 1_arrays_and_hashing/9_longest_consecutive_sequence.py
+++ 1_arrays_and_hashing/9_longest_consecutive_sequence.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
 from typing import List
 
 
@@ -7,7 +8,7 @@ class Solution:
         max_length = 0
         for num in nums:
             # check if it's a start of a sequence
-            if num - 1 not in nums_set:
+            if num - 1 not in nums_set:  # O(1)
                 length = 1
                 while num + length in nums_set:
                     length += 1
@@ -16,6 +17,7 @@ class Solution:
 
         return max_length
 
+
 if __name__ == '__main__':
     nums = [100, 4, 200, 1, 3, 2, 5]  # 5
     # nums = [100, 4, 200, 1, 3, 2]  # 4
@@ -23,3 +25,16 @@ if __name__ == '__main__':
     # nums = [0, 3, 7, 2, 5, 8, 4]  # 4
     solution = Solution()
     print(solution.longestConsecutive(nums))
+
+# [1, 2, 3, 4]
+# 1 - начало последовательности, значит будет еще + 3 итерации
+# 2 - не начало
+# 3 - не начало
+# 4 - не начало
+
+
+# [1, 3, 5, 6]
+# 1 - начало, но по while сразу вылетим
+# ..
+# ..
+# ..
